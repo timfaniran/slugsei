be13ac2bf544e5847c05e7033d0b11f93b83f1b1
Timothy Faniran <44296109+timfaniran@users.noreply.github.com>
2025-02-12 18:05:50 -0600

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:8080';  
+const BASE_URL = 'https://virtual-baseball-coach-5trazf644q-uc.a.run.app'; // local: http://127.0.0.1:8080  
 
 const handleError = (error) => {
     console.error(error);
